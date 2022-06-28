576e6a27cdf71dca438e0de30fb9daf86a74191e
Minty20090 <44348348+Minty20090@users.noreply.github.com>
2022-06-28 12:18:11 -0700

    working nav

---
+++ script.js
@@ -0,0 +1,8 @@
+var a = 0;
+var num = document.getElementById('num');
+function counter () {
+    a += 5;
+    num.innerHTML = a + "+";
+}
+setInterval(counter(), 1000);
+x = setInterval(function () {num.innerHTML = a +"+"; a+=5; if (a==16005){clearInterval(x)}}, .01);
