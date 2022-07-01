0f7577e55bb075301a7dc44df8d540615be874df
Minty20090 <44348348+Minty20090@users.noreply.github.com>
2022-06-30 21:47:34 -0700

    final(some filler text)

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Variables
 var a = 0;
 var num = document.getElementById('num');
-
-setInterval(counter(), 1000);
-x = setInterval(function () {num.innerHTML = a +"+"; a+=5; if (a==16005){clearInterval(x)}}, .01);
+x = setInterval(function () {num.innerHTML = a +"+"; a+=25; if (a>=16020){clearInterval(x)}}, .1);
